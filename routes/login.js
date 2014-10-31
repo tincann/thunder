589776770c3274fe3a54e1ce0998841a264da92b
danielbroy <daniel.roy@infi.nl>
2014-10-31 16:02:13 +0100

    Toevoegen user create

--- routes/login.js
+++ routes/login.js
@@ -26,29 +26,49 @@ router.get('/logout', function(req, res) {
 // Selecteert een al bekende facebook id en verwijst door naar het statusscherm.
 router.post('/select', function (req,res) {
     // Is de meegegeven id bekend?
-    var selected_id = parseInt(req.param('fbid', 0));
+    var selected_id = parseInt(req.param('fbid', 0), 10);
     var selected_account = req.db.FacebookAccounts.findOne({FacebookId: selected_id}, function (err, result) {
         if(result === null) {
             // Onbekende account -> terug naar loginpagina.
             req.session.last_error = "Het geselecteerde account is niet gevonden.";
+            req.session.user = null;
             res.redirect('/login');
         } else {
             // Account gevonden: zetten login in de sessie en doorverwijzen status pagina.
             req.session.last_error = "";
-            req.session.user = {fbid: selected_id, fbalias: result.alias};
+            req.session.user = {fbid: selected_id, fbalias: result.Alias};
             res.redirect('/status');
         }
     });
  });
 
 router.post('/create', function(req,res) {
-   res.send('create - fbid: ' + req.param('newfbid') + ' - fbtoken: ' + req.param('newfbtoken'));
+     // Checken input
+    var fbalias = req.param('newfbalias');
+    var fbtoken = req.param('newfbtoken');
+    var fbid = parseInt(req.param('newfbid'), 10);
 
-   // TODO
-    // geldinge input?
-    // Nee, dan terugverwijzen naar login -- TODO: foutmeldingen
-    // Ja, dan aanmaken account, zetten cookie en doorverwijzen naar statuspagina.
-
+    if (!fbalias || fbid == Number.NaN || fbid < 0 || !fbtoken) {
+        // Input is ongeldig, terugsturen naar loginscherm.
+        req.session.last_error = "De ingevoerde gegevens zijn ongeldig.";
+        req.session.user = null;
+        res.redirect('/login');
+    } else {
+        // Aanmaken nieuw account.
+        req.db.FacebookAccounts.Insert( {FacebookId: fbid, FacebookToken: fbtoken, Alias: fbalias}, function(error, result){
+            // Fout bij aanmaken account, terugsturen naar loginscherm.
+            if (error) {
+                req.session.last_error = "Er was een fout bij het aanmaken van het nieuwe account: " + error.message;
+                req.session.user = null;
+                res.redirect('/login');
+            } else {
+                // Zetten login in de sessie en doorverwijzen status pagina.
+                req.session.last_error = "";
+                req.session.user = {'fbid': fbid, 'fbalias': fbalias};
+                res.redirect('/status');
+            }
+        });
+    }
 });
 
 module.exports = router;
