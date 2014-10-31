4f5389d0bfa613d46f094ffaa372c86bc8dc2cb6
DanielBronk <danielbronk@hotmail.com>
2014-10-31 15:55:04 +0100

    Filter view

--- routes/filters.js
+++ routes/filters.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* Verwijst door naar de default pagina (login). */
+/* View met alle filters voor de match */
 router.get('/', function (req, res) {
     res.render('filters');
-} );
+});
 
 module.exports = router;
