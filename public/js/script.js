a2c5295859cc37acf7b8b545db305fadc3286688
DanielBronk <danielbronk@hotmail.com>
2014-10-31 21:27:38 +0100

    filter vars

--- public/js/script.js
+++ public/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         range: true,
         min: 18,
         max: 99,
-        values: [ 25, 35 ],
+        values: [ $('#age_min').val(), $('#age_max').val() ],
         slide: function( event, ui ) {
             $('#age_min').val(ui.values[0]);
             $('#age_max').val(ui.values[1]);
@@ -31,7 +31,7 @@ $(document).ready(function(){
     $('#range').slider({
         min: 0,
         max: 500,
-        values: [50],
+        values: [$('#rangekm').val()],
         slide: function( event, ui ) {
             $('#rangekm').val(ui.values[0]);
         }
