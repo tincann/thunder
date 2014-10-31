7832ba334054f7f6048b17bb5f46334a0513f44c
DanielBronk <danielbronk@hotmail.com>
2014-10-31 18:23:54 +0100

    filters

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,29 @@
 
 $(document).ready(function(){
 
+    //geslacht radio buttons
     $('#gender').buttonset();
+
+    //leeftijd slider
+    $('#age').slider({
+        range: true,
+        min: 18,
+        max: 99,
+        values: [ 25, 35 ],
+        slide: function( event, ui ) {
+            //$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+
+    //Ophalen van lat en lng en autocomplete adres
+    var autocomplete = new google.maps.places.Autocomplete((document.getElementById('location_input')),{ types: ['geocode'] });
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        console.log(place.geometry.location.lat());
+        console.log(place.geometry.location.lng());
+    });
+
+    //Afstand slider
+    $('#range').slider();
 
 });
