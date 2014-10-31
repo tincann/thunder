f3128028c8baf3e48b8ca78cfd734e093a1b5818
tincann <saabnl@gmail.com>
2014-10-31 21:47:08 +0100

    Like alle matches

--- bot/SearchUpdateBot.js
+++ bot/SearchUpdateBot.js
@@ -1,10 +1,18 @@
+var db = require('../thunder-db').database;
+
+var STATUS = {
+    IDLE: 0,
+    RUNNING: 1
+};
 
 function SearchUpdateBot(pollInterval){
-    
+    this.interval = setInterval(this.loop.bind(this), pollInterval || 660
+
 }
 
 SearchUpdateBot.prototype.loop = function() {
-    
+    console.log('getting tinder updates');
+
 };
 
 module.exports = new SearchUpdateBot();
